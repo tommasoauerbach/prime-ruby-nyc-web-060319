9ab1733529546da32769200ca10e80a6c663cd6d
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 20:14:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(num)
-  if (num > 2) && (num % num == 0)
-    puts true
+  if num < 0 or num == 0 or num ==1
+    return false
+  else
+    (2...num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
-  puts false
 end
+puts prime?(105557)
